2aa9471ae6fc4e401ad9da3da5acb8c88a87c117
Kaio Sousa <kos20111@hotmail.com>
2025-03-19 00:53:08 -0300

    Correção busca ordenada

--- ordenada.cpp
+++ ordenada.cpp
@@ -1,7 +1,6 @@
 #include "grafo.h"
 #include <fstream>
 #include <chrono>
-#include <algorithm>
 #include <limits>
 
 using namespace std;
@@ -9,53 +8,32 @@ using namespace std;
 pair<int, int> Grafo::ordenada(string arq) {
     auto start = chrono::high_resolution_clock::now();
 
-    vector<int> rota(num_vertices, -1);
-    vector<bool> visitada(num_vertices, false);
-    rota[0] = 0;
-    visitada[0] = true;
-
-    int solAchada = 0;
-    int nosVisitados = 1;
-
-    for (int i = 1; i < num_vertices; i++) {
-        int cidadeAtual = rota[i - 1];
-        int melhorVizinho = -1;
-        int menorDistancia = numeric_limits<int>::max();
-
-        for (int j = 0; j < num_vertices; j++) {
-            if (!visitada[j] && dist[cidadeAtual][j] < menorDistancia) {
-                melhorVizinho = j;
-                menorDistancia = dist[cidadeAtual][j];
-            }
-        }
-
-        if (melhorVizinho == -1) {
-            solAchada = numeric_limits<int>::max();
-            break;
-        }
-
-        rota[i] = melhorVizinho;
-        visitada[melhorVizinho] = true;
-        solAchada += menorDistancia;
-        nosVisitados++;
+    // Criar a rota percorrendo os vértices sequencialmente
+    vector<int> rota(num_vertices);
+    for (int i = 0; i < num_vertices; i++) {
+        rota[i] = i;
     }
 
+    int solAchada = 0;
+    int nosVisitados = num_vertices;
+
+    // Calcular o custo total do caminho percorrido
+    for (int i = 0; i < num_vertices - 1; i++) {
+        solAchada += dist[rota[i]][rota[i + 1]];
+    }
     solAchada += dist[rota[num_vertices - 1]][rota[0]]; // Fechando o ciclo
-    
+
+    // Salvar a solução encontrada em um arquivo
     ofstream fout("source/" + arq);
     fout << num_vertices << "\n";
-    for (int i = 0; i < num_vertices - 1; i++)
+    for (int i = 0; i < num_vertices - 1; i++) {
         fout << rota[i] + 1 << " " << rota[i + 1] + 1 << " " << dist[rota[i]][rota[i + 1]] << "\n";
+    }
     fout << rota.back() + 1 << " " << rota[0] + 1 << " " << dist[rota.back()][rota[0]] << "\n";
-
-    if (solAchada == numeric_limits<int>::max())
-        cout << "Fracasso!\n";
-    else 
-        cout << "Sucesso!\n";
 
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> duration = end - start;
     cout << "Tempo de execucao: " << duration.count() << " ms" << endl;
-    
+
     return {solAchada, nosVisitados};
 }
